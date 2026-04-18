337ce6324d91e1fccc2d9f096ec2ad38cf0e172e
Amadej Glasenčnik <amadej.glasencnik@gmail.com>
2026-04-18 22:09:34 +0200

    Install roslyn nuget packages in generator project and add empty generator class

---
+++ src/TypedIcons.Generator/IconGenerator.cs
@@ -0,0 +1,12 @@
+using Microsoft.CodeAnalysis;
+
+namespace TypedIcons.Generator;
+
+[Generator]
+public class IconGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // TODO
+    }
+}
