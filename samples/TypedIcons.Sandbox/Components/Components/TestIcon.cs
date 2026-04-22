a9b841e8e55317a54098a1d4a26dea8cf16bdc5e
Amadej Glasenčnik <amadej.glasencnik@gmail.com>
2026-04-22 14:24:24 +0200

    Update test icon component

--- samples/TypedIcons.Sandbox/Components/Components/TestIcon.cs
+++ samples/TypedIcons.Sandbox/Components/Components/TestIcon.cs
@@ -6,22 +6,59 @@ namespace TypedIcons.Sandbox.Components.Components;
 public partial class TestIcon : ComponentBase
 {
     [Parameter] public TestIconData? Value { get; set; }
+    
+    [Parameter] public string? Size { get; set; }
+    
+    [Parameter] public string? Width { get; set; }
+    
+    [Parameter] public string? Height { get; set; }
 
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        if (Value is null) return;
+        if (Value is null) 
+            return;
         
         var iconData = Value.Value;
+        var (width, height) = CalculateDimensions(iconData);
         
         builder.OpenElement(0, "svg");
         builder.AddAttribute(1, "xmlns", "http://www.w3.org/2000/svg");
         builder.AddAttribute(2, "viewBox", iconData.ViewBox);
-        builder.AddAttribute(3, "width", iconData.Width);
-        builder.AddAttribute(4, "height", iconData.Height);
+        builder.AddAttribute(3, "width", width);
+        builder.AddAttribute(4, "height", height);
         builder.AddAttribute(5, "fill", "currentColor");
 
-        builder.AddMarkupContent(6, Value.Value.Content);
+        builder.AddMarkupContent(6, iconData.Content);
 
         builder.CloseElement();
+    }
+
+    private (string, string) CalculateDimensions(TestIconData iconData)
+    {
+        string width, height;
+        
+        if (Size is not null)
+        {
+            width = height = Size;
+        }
+        else if (Width is not null && Height is not null)
+        {
+            width = Width;
+            height = Height;
+        }
+        else if (Width is not null)
+        {
+            width = height = Width;
+        }
+        else if (Height is not null)
+        {
+            width = height = Height;
+        }
+        else
+        {
+            width = height = iconData.Width;
+        }
+        
+        return (width, height);
     }
 }
