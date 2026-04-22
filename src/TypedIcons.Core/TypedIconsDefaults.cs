dd177122436085efe639f5741ba0c1e5c5f7b35b
Amadej Glasenčnik <amadej.glasencnik@gmail.com>
2026-04-22 17:44:05 +0200

    Convert defaults into constants

--- src/TypedIcons.Core/TypedIconsDefaults.cs
+++ src/TypedIcons.Core/TypedIconsDefaults.cs
@@ -2,9 +2,9 @@ namespace TypedIcons.Core;
 
 public static class TypedIconsDefaults
 {
-    public static readonly string ConfigFileName = "typedicons.json";
+    public const string ConfigFileName = "typedicons.json";
     
-    public static readonly string CacheFolderName = "TypedIcons";
+    public const string CacheFolderName = "TypedIcons";
 
-    public static readonly string CacheFileName = "cache.json";
+    public const string CacheFileName = "cache.json";
 }
